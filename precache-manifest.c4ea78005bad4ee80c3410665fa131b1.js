fa6ccc217e60673d5397b6fa6ccac7ba4ece8dd2
egorspartik <58834432+egorspartik@users.noreply.github.com>
2020-07-20 22:09:48 +0700

    Updates

--- precache-manifest.c4ea78005bad4ee80c3410665fa131b1.js
+++ precache-manifest.c4ea78005bad4ee80c3410665fa131b1.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "08d9ff04be7fd450825f8c69faf9569c",
+    "revision": "b1acb3f286de7f086158d6da06e872ce",
     "url": "./index.html"
   },
   {
-    "revision": "3b65988438788e4ed6fb",
+    "revision": "cc439981a437ad2c797b",
     "url": "./static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "55bbe24a20fc2476efce",
+    "revision": "5766e030a6d5fffd0527",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "3b65988438788e4ed6fb",
-    "url": "./static/js/2.14299950.chunk.js"
+    "revision": "cc439981a437ad2c797b",
+    "url": "./static/js/2.f1f92abf.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "./static/js/2.14299950.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.f1f92abf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "55bbe24a20fc2476efce",
-    "url": "./static/js/main.daa81b9b.chunk.js"
+    "revision": "5766e030a6d5fffd0527",
+    "url": "./static/js/main.4972bb97.chunk.js"
   },
   {
     "revision": "fb54b6bed906a3e09b6a",
