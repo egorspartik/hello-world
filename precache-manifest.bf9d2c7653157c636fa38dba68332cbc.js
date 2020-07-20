fb7053e9ff0a02f9c1d04993838912284b420583
egorspartik <58834432+egorspartik@users.noreply.github.com>
2020-07-20 22:01:54 +0700

    Updates

---
+++ precache-manifest.bf9d2c7653157c636fa38dba68332cbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d9ff04be7fd450825f8c69faf9569c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b65988438788e4ed6fb",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "55bbe24a20fc2476efce",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3b65988438788e4ed6fb",
+    "url": "./static/js/2.14299950.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.14299950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55bbe24a20fc2476efce",
+    "url": "./static/js/main.daa81b9b.chunk.js"
+  },
+  {
+    "revision": "fb54b6bed906a3e09b6a",
+    "url": "./static/js/runtime-main.54d55149.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbf3da1c52cef17774c42009f34518cd.js"
+  "./precache-manifest.bf9d2c7653157c636fa38dba68332cbc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
